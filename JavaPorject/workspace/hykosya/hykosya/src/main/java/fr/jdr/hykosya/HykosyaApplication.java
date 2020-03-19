9b5bbfe96df3a1d10caab812ee520f0bd3d48c06
SONFILS <david.sonfils@capgemini.com>
2020-03-19 16:28:35 +0100

    Refonte  java Spring Boot Angular

---
+++ JavaPorject/workspace/hykosya/hykosya/src/main/java/fr/jdr/hykosya/HykosyaApplication.java
@@ -0,0 +1,13 @@
+package fr.jdr.hykosya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HykosyaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HykosyaApplication.class, args);
+    }
+
+}
---
+++ JavaPorject/workspace/hykosya/hykosya/src/test/java/fr/jdr/hykosya/HykosyaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.jdr.hykosya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HykosyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
